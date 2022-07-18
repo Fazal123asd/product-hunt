c5f189790f18ce306da47bc0a8b74ebec0028658
Fazal123asd <41240844+Fazal123asd@users.noreply.github.com>
2022-07-18 16:37:12 +0530

    Update tailwind.config.js

--- apps/product-hunt/tailwind.config.js
+++ apps/product-hunt/tailwind.config.js
@@ -1,8 +1,31 @@
-/** @type {import('tailwindcss').Config} */
+const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
+
+const { join } = require('path');
+
+
+
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+
+content: [
+
+join(
+
+__dirname,
+
+'{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+
+),
+
+...createGlobPatternsForDependencies(__dirname),
+
+],
+
+theme: {
+
+extend: {},
+
+},
+
+plugins: [],
+
+};
